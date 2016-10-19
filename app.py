ca0846b7bd6e22da067b54234e86d910bd9767ff
FRANKIE\Nam Hai <namhai.lsc.ftu@gmail.com>
2016-10-19 20:46:55 +0700

    Demo

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
+import json
 from flask import Flask
 
 app = Flask(__name__)
 
+post1 = {
+    "title": "Good day",
+    "content": "Today I met a girl. Fking noob"
+}
+
+post2 = {
+    "title": "Bad day",
+    "content": "Today I met a Dumb Boy. Fking Retard"
+}
+
+print(post1["title"])
+print(post1["content"])
+
+print(post2["title"])
+print(post2["content"])
+
+posts = [post1, post2]
 
 @app.route('/')
 def hello_world():
-    return 'Hi Anh Hai!'
+    return json.dumps(posts)
 
 
 if __name__ == '__main__':
